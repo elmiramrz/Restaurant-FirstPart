e87ae1966c9cd3ca1bafa924a6f3433fbd8e956d
elmiramrz <mirza_elmira@yahoo.com>
2021-02-04 23:48:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-/********************************************************************************** 
-WEB422 – Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.* 
-* Name: Elmira Mirza     Student ID: 130828197     Date: 2020/18/01
-* Heroku Link: https://git.heroku.com/shrouded-cove-94858.git
-*
-********************************************************************************/
+/////https://salty-taiga-06312.herokuapp.com
+
 
 
 const express = require("express");
